41f49e7ce006f1e3367dc44ac682adca3c55b10b
ahmetserdargeze <gezeahmetserdar@gmail.com>
2020-08-22 12:58:21 +0300

    Empty project run and first screen added

---
+++ views/FirstSceen.tsx
@@ -0,0 +1,16 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function FirsScreen() {
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.tsx to start working on your app!</Text>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {},
+});
